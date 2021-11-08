e5be50871c1e6325a3dc5a3f7c63b57b0a1d52b1
asiryan <dmc5mod@yandex.ru>
2021-11-08 07:55:12 +0300

    Refactoring and version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 # package metadata
 NAME = 'caffe2onnx'
-VERSION = '1.1.2'
+VERSION = '1.1.3'
 DESCRIPTION = 'Convert Caffe models to ONNX.'
 LICENSE = 'BSD-3'
 GIT = 'https://github.com/asiryan/caffe-onnx'
